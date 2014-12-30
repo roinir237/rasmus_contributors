0034c64b496d28fda3ddb3808e4e29dd69866ab8
roinir237 <roinir237@gmail.com>
2014-12-30 12:18:46 +0000

    initialize capifony

---
+++ app/config/deploy.rb
@@ -0,0 +1,20 @@
+set :application, "rasmus contributors"
+set :domain,      "ec2-50-112-141-57.us-west-2.compute.amazonaws.com"
+set :deploy_to,   "/var/www/contributors"
+set :app_path,    "app"
+
+set :scm,           :git
+set :repository,    "git@github.com:roinir237/rasmus_contributors.git"
+
+set :model_manager, "doctrine"
+
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,        domain, :primary => true       # This may be the same as your `Web` server
+
+set :use_sudo,      true
+set :keep_releases,  3
+
+set :shared_files,      ["app/config/parameters.yml"]
+set :shared_children,     [app_path + "/logs", web_path + "/uploads", "vendor"]
+set :use_composer, true
+set :update_vendors, true
